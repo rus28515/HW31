d7a8385e10223ddeb9816a8ec9d4d21608fcee69
Runo28 <runo283@gmail.com>
2022-11-08 20:19:30 +0200

    2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,50 +12,8 @@ public class App {
 
     public void startMenu() {
         Scanner scn = new Scanner(System.in);
-//        System.out.println("1. Добавить нового пользователя.");
-//        System.out.println("2. Удалить существующего пользователя.");
-//        System.out.println("3. Проверить существует ли пользователь.");
-//        System.out.println("4. Изменить логин существующего пользователя.");
-//        System.out.println("5. Изменить пароль существующего пользователя.");
-//        System.out.println("6. Выход");
-//        int tmp = scn.nextInt();
-//        switch (tmp) {
-//            case 1:
-//                logging.add();
-//                startMenu();
-//                break;
-//            case 2:
-//                logging.remove();
-//                startMenu();
-//                break;
-//            case 3:
-//                logging.contains();
-//                startMenu();
-//                break;
-//            case 4:
-//                logging.changeLogin();
-//                startMenu();
-//                break;
-//            case 5:
-//                logging.changePassword();
-//                startMenu();
-//                break;
-//            case 6:
-//                System.out.println("Сеанс завершено");
-//                break;
-//            case 7:
-//                logging.printLogin();
-//                logging.printPas();
-//                startMenu();
-//                break;
-//            default:
-//                System.out.println("Не вірно ведено пункт");
-//                startMenu();
-//
-//        }
-
-        boolean b = true;
-        while (b){
+        boolean res = true;
+        while (res) {
             System.out.println("1. Добавить нового пользователя.");
             System.out.println("2. Удалить существующего пользователя.");
             System.out.println("3. Проверить существует ли пользователь.");
@@ -63,25 +21,25 @@ public class App {
             System.out.println("5. Изменить пароль существующего пользователя.");
             System.out.println("6. Выход");
             int tmp = scn.nextInt();
-            if (tmp ==1){
+            if (tmp == 1) {
                 logging.add();
-            } else if (tmp==2) {
+            } else if (tmp == 2) {
                 logging.remove();
-            } else if (tmp==3) {
+            } else if (tmp == 3) {
                 logging.contains();
-            } else if (tmp==4) {
+            } else if (tmp == 4) {
                 logging.changeLogin();
-            } else if (tmp==5) {
+            } else if (tmp == 5) {
                 logging.changePassword();
-            } else if (tmp==6) {
+            } else if (tmp == 6) {
                 System.out.println("Сеанс завершено");
-                b=false;
-            } else if (tmp==7) {
+                res = false;
+            } else if (tmp == 7) {
 // Для перевірки введених даних
                 logging.printLogin();
                 logging.printPas();
             } else {
-                System.out.println("Не вірно ведено пункт");
+                System.out.println("Невірно введено пункт");
             }
         }
     }
